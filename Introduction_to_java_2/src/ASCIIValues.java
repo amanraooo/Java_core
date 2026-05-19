37874efddf90b8316783fc9df294104bc88b6708
Aman Gaade <ak9070777@gmail.com>
2026-05-19 12:19:38 +0530

    ASCII Values

---
+++ Introduction_to_java_2/src/ASCIIValues.java
@@ -0,0 +1,15 @@
+public class ASCIIValues {
+	public static void main(String[] args) {
+		System.out.println("Big Alphabets");
+		for (int i = 'A'; i <= 'Z'; i++) {
+			System.out.println(((char) i) + " " + i + " ");
+		}
+
+		System.out.println();
+
+		System.out.println("Small Alphabets");
+		for (int i = 'a'; i <= 'z'; i++) {
+			System.out.println(((char) i) + " " +i + " ");
+		}
+	}
+}
