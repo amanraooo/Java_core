7c6dbce343def751a6d1e64e54c122b5aefea4ed
Aman Gaade <ak9070777@gmail.com>
2026-05-19 12:26:09 +0530

    VariableDemo1

--- Introduction_to_java_2/src/VariableDemo1.java
+++ Introduction_to_java_2/src/VariableDemo1.java
@@ -1,2 +1,8 @@
 public class VariableDemo1 {
+	public static void main(String[] args) {
+		int salary = 10000;
+		String name = "Rohit";
+		System.out.println("My salary is = " + salary);
+		System.out.println("My name is = " + name);
+	}
 }
