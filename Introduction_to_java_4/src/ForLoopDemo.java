8954324db1cf7b59f2a62a2c7641fe0fbeb4017d
Aman Gaade <ak9070777@gmail.com>
2026-05-25 12:48:09 +0530

    ForLoopDemo

--- Introduction_to_java_4/src/ForLoopDemo.java
+++ Introduction_to_java_4/src/ForLoopDemo.java
@@ -1,2 +1,12 @@
 public class ForLoopDemo {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Aman");
+		}
+		// 0
+		// 1
+		// 2
+		// 3
+		// 4
+	}
 }
