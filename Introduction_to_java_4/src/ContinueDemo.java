1f997f77dc1682f924c069274f7a561947e4d3ad
Aman Gaade <ak9070777@gmail.com>
2026-05-25 12:44:19 +0530

    ContinueDemo

--- Introduction_to_java_4/src/ContinueDemo.java
+++ Introduction_to_java_4/src/ContinueDemo.java
@@ -1,2 +1,10 @@
 public class ContinueDemo {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			if (i == 5) { // true false
+				continue;
+			}
+			System.out.println("Hello World! " + i);
+		}
+	}
 }
