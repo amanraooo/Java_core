d9d4044e553826c199983af9532de602187d78ec
Aman Gaade <ak9070777@gmail.com>
2026-06-11 17:18:13 +0530

    throw keyword

--- Exception_Handle/src/throwDemo/ThrowTest.java
+++ Exception_Handle/src/throwDemo/ThrowTest.java
@@ -1,4 +1,30 @@
 package throwDemo;
 
 public class ThrowTest {
+	public static void main(String[] args) {
+		try
+		{
+			withdraw(1000,2000);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error "+e.getMessage());
+		}
+
+	}
+
+	public static void withdraw(double bal,double amt) throws Exception {
+		if(amt>bal)
+		{
+			throw new Exception("Insufficent Balance");
+            /*try {
+                throw new Exception("Insufficent Balance");
+            }
+            catch (Exception e)
+            {
+
+            }*/
+		}
+		System.out.println("withdrawal successful...");
+	}
 }
