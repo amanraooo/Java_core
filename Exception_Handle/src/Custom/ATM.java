c45012cbf4bc8f09d58d6bd2e4dc3795b78dde84
Aman Gaade <ak9070777@gmail.com>
2026-06-11 17:25:25 +0530

    custom exception

--- Exception_Handle/src/Custom/ATM.java
+++ Exception_Handle/src/Custom/ATM.java
@@ -4,16 +4,16 @@ public class ATM {
 	public static void main(String[] args) {
 		try {
 			withdraw(1000,1500);
-		} catch (InsufficentBalExeption e) {
+		} catch (InsufficentBalException e) {
 			System.out.println("Exception Bal ins");
 			System.out.println(e);
 		}
 	}
 
-	public static void withdraw(double bal,double amt) throws InsufficentBalExeption {
+	public static void withdraw(double bal,double amt) throws InsufficentBalException {
 		if(amt>bal)
 		{
-			throw new InsufficentBalExeption("Insufficient Bal : " +
+			throw new InsufficentBalException("Insufficient Bal : " +
 					"Available bal "+bal+ "requested "+amt);
 
 		}
