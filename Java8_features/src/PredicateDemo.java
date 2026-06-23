cd28a5fb61a81297209883cf0b04cf4496dc71f0
Aman Gaade <ak9070777@gmail.com>
2026-06-23 13:27:39 +0530

    PredicateDemo

--- Java8_features/src/PredicateDemo.java
+++ Java8_features/src/PredicateDemo.java
@@ -1,2 +1,12 @@
+import java.util.function.Predicate;
+
 public class PredicateDemo {
+
+	public static void main(String[] args) {
+
+		Predicate<Integer> even = n -> n % 2 == 0;
+
+		System.out.println(even.test(10));
+		System.out.println(even.test(11));
+	}
 }
