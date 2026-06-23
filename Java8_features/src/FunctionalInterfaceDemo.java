f9a1d66233230d8bf43f6992244bb977b704a38e
Aman Gaade <ak9070777@gmail.com>
2026-06-23 13:24:15 +0530

    FunctionalInterfaceDemo

--- Java8_features/src/FunctionalInterfaceDemo.java
+++ Java8_features/src/FunctionalInterfaceDemo.java
@@ -1,2 +1,13 @@
+@FunctionalInterface
+interface Square {
+	int square(int n);
+}
+
 public class FunctionalInterfaceDemo {
+	public static void main(String[] args) {
+
+		Square s = x -> x * x;
+
+		System.out.println(s.square(5));
+	}
 }
