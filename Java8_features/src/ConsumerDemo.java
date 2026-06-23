ddead6a664a1f9fbd98502cc921870950d01c7e2
Aman Gaade <ak9070777@gmail.com>
2026-06-23 13:35:38 +0530

    ConsumerDemo

--- Java8_features/src/ConsumerDemo.java
+++ Java8_features/src/ConsumerDemo.java
@@ -1,2 +1,11 @@
+import java.util.function.Consumer;
+
 public class ConsumerDemo {
+
+	public static void main(String[] args) {
+
+		Consumer<String> c = System.out::println;
+
+		c.accept("Hello");
+	}
 }
