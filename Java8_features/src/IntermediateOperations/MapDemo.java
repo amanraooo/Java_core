6aef13f92f3e8bcc3d643eaa071724c9f2964a36
Aman Gaade <ak9070777@gmail.com>
2026-06-24 12:36:20 +0530

    map

---
+++ Java8_features/src/IntermediateOperations/MapDemo.java
@@ -0,0 +1,12 @@
+package IntermediateOperations;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapDemo {
+	List<Integer> list = Arrays.asList(10,15,20,25,30);
+
+      list.stream()
+		.filter(n -> n % 2 == 0)
+			.forEach(System.out::println);
+}
