10d483809a57fe8aa6581d4fc9a3b0c1e22ea79e
Aman Gaade <ak9070777@gmail.com>
2026-06-24 14:03:23 +0530

    limit

--- Java8_features/src/IntermediateOperations/LimitDemo.java
+++ Java8_features/src/IntermediateOperations/LimitDemo.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class LimitDemo {
-	     Stream.iterate(1,n->n+1)
-			.limit(5)
-      .forEach(System.out::println);
+	public static void main(String[] args) {
+		Stream.iterate(1,n->n+1)
+				.limit(5)
+				.forEach(System.out::println);
+	}
+
 }
