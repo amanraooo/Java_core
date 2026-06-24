24600dc8e6c8201eab57fcd981075d1ab72551df
Aman Gaade <ak9070777@gmail.com>
2026-06-24 12:48:03 +0530

    limit

--- Java8_features/src/IntermediateOperations/LimitDemo.java
+++ Java8_features/src/IntermediateOperations/LimitDemo.java
@@ -2,11 +2,10 @@ package IntermediateOperations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class LimitDemo {
-	List<Integer> list = Arrays.asList(10,20,10,30,20);
-
-     list.stream()
-		.distinct()
-    .forEach(System.out::println);
+	     Stream.iterate(1,n->n+1)
+			.limit(5)
+      .forEach(System.out::println);
 }
