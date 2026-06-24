05f62b8a9589fc54bd790f0d1dce61289d6f942a
Aman Gaade <ak9070777@gmail.com>
2026-06-24 13:57:33 +0530

    streamOf

--- Java8_features/src/StreamCreation/StreamOfDemo.java
+++ Java8_features/src/StreamCreation/StreamOfDemo.java
@@ -3,6 +3,10 @@ package StreamCreation;
 import java.util.stream.Stream;
 
 public class StreamOfDemo {
-	Stream.of("A","B","C")
-			.forEach(System.out::println);
+
+	public static void main(String[] args) {
+		Stream.of("A","B","C")
+				.forEach(System.out::println);
+	}
+
 }
