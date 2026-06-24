66efc86adb5da6f0e618663333cfd6e430cd52cd
Aman Gaade <ak9070777@gmail.com>
2026-06-24 13:48:53 +0530

    distinct

--- Java8_features/src/IntermediateOperations/DistinctDemo.java
+++ Java8_features/src/IntermediateOperations/DistinctDemo.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DistinctDemo {
-	List<Integer> list = Arrays.asList(10,20,10,30,20);
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(10,20,10,30,20);
 
-      list.stream()
-		.distinct()
-    .forEach(System.out::println);
+		list.stream()
+				.distinct()
+				.forEach(System.out::println);
+	}
+
 }
