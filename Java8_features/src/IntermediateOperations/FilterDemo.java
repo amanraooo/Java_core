e3619edf9b27ab70f641088036ca5d26cbe4aa54
Aman Gaade <ak9070777@gmail.com>
2026-06-24 12:34:43 +0530

    filter

--- Java8_features/src/IntermediateOperations/FilterDemo.java
+++ Java8_features/src/IntermediateOperations/FilterDemo.java
@@ -1,4 +1,12 @@
 package IntermediateOperations;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FilterDemo {
+	List<Integer> list = Arrays.asList(10,15,20,25,30);
+
+         list.stream()
+		.filter(n -> n % 2 == 0)
+			.forEach(System.out::println);
 }
