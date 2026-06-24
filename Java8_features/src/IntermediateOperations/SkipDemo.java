82358ead8f32b2d5af666884b672e15567af2d79
Aman Gaade <ak9070777@gmail.com>
2026-06-24 12:50:21 +0530

    skip

--- Java8_features/src/IntermediateOperations/SkipDemo.java
+++ Java8_features/src/IntermediateOperations/SkipDemo.java
@@ -1,5 +1,12 @@
 package IntermediateOperations;
 
-public class SkipDemo {
+import java.util.Arrays;
+import java.util.List;
 
+public class SkipDemo {
+	List<Integer> list = Arrays.asList(10,20,30,40,50);
+
+     list.stream()
+		.skip(2)
+    .forEach(System.out::println);
 }
