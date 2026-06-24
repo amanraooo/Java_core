3799f514b1a7255b89b139c2de744433d5ced403
Aman Gaade <ak9070777@gmail.com>
2026-06-24 14:35:18 +0530

    LocalDateTime

---
+++ Java8_features/src/DateAndTimeAPI/LocalDateTIme.java
@@ -0,0 +1,12 @@
+package DateAndTimeAPI;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTIme {
+	public static void main(String[] args) {
+		LocalDateTime dt = LocalDateTime.now();
+
+		System.out.println(dt);
+	}
+
+}
--- Java8_features/src/IntermediateOperations/FilterDemo.java
+++ Java8_features/src/IntermediateOperations/FilterDemo.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FilterDemo {
-	List<Integer> list = Arrays.asList(10,15,20,25,30);
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(10,15,20,25,30);
 
-         list.stream()
-		.filter(n -> n % 2 == 0)
-			.forEach(System.out::println);
+		list.stream()
+				.filter(n -> n % 2 == 0)
+				.forEach(System.out::println);
+	}
+
 }
