40a2bc55be1d7e530c50c715e1983e57841c684d
Aman Gaade <ak9070777@gmail.com>
2026-06-24 13:42:11 +0530

    LocalDate

--- Java8_features/src/DateAndTimeAPI/LocalDateDemo.java
+++ Java8_features/src/DateAndTimeAPI/LocalDateDemo.java
@@ -1,4 +1,11 @@
 package DateAndTimeAPI;
 
+import java.time.LocalDate;
+
 public class LocalDateDemo {
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+
+		System.out.println(date);
+	}
 }
