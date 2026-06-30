8e6325378f61652ea9072b95545ff52b828ab3e2
Aman Gaade <ak9070777@gmail.com>
2026-06-30 13:33:47 +0530

    Booking Service

---
+++ BookMyTrain/src/BookingService.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingService {
+
+	private List<Train> trainList= new ArrayList<>();
+	private List<Ticket> ticketList= new ArrayList<>();
+
+	public BookingService(){
+		trainList.add(new Train(101,"Rajdhaani Express","Delhi", "Nagpur",100));
+		trainList.add(new Train(102,"Vande Bharat","Delhi", "Mumbai",60));
+		trainList.add(new Train(103,"Tejas Express","Delhi", "Pune",60));
+		trainList.add(new Train(104,"Jhelum Express","Kolkata", "Bengaluru",10));
+		trainList.add(new Train(105,"Local Express","Delhi", "Agra",87));
+
+	}
+
+	public List<Train> searchTrain(String source, String destination){
+
+		List<Train> searchResult = new ArrayList<>();
+
+		for (Train train : trainList){
+
+			if(train.getSource().equalsIgnoreCase(source) && train.getDestination().equalsIgnoreCase(destination)){
+				searchResult.add(train);
+			}
+		}
+		return searchResult;
+	}
+
+}
