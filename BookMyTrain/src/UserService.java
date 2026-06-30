6962d749114754e3f53f867c01abd332f9a5d97c
Aman Gaade <ak9070777@gmail.com>
2026-06-30 13:04:54 +0530

    User Service

--- BookMyTrain/src/UserService.java
+++ BookMyTrain/src/UserService.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserService {
+
+	private Map<String,User> userMap = new HashMap<>();
+
+	private User currentUser = null;
+
+	public boolean registerUser(String username, String password, String fullname, String contact){
+		if (userMap.containsKey(username)) {
+
+			System.out.println("Username already taken, please choose another username");
+		}
+
+		User user = new User(username, password, fullname, contact);
+		userMap.put(username,user);
+		System.out.println("Registration Succesfull : )");
+
+		return true;
+	}
 }
