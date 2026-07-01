3a90fc9dd8ed52bbbec06fe48c970116bd74148e
Aman Gaade <ak9070777@gmail.com>
2026-07-01 13:05:38 +0530

    APP

--- BookMyTrain/src/IRCTCAPP.java
+++ BookMyTrain/src/IRCTCAPP.java
@@ -27,8 +27,30 @@ public class IRCTCAPP {
 
 				switch (choice){
 					case 1 -> register();
+					case 2 -> login();
+					case 3 -> exitApp();
+					default -> System.out.println("Invalid choice");
 				}
 			}
 		}
 	}
+
+	public void register(){
+		System.out.println("Enter Username: ");
+		String username = scanner.next();
+
+		System.out.println("Enter Password: ");
+		String password = scanner.next();
+
+		System.out.println("Enter full name: ");
+		scanner.nextLine();
+		String fullname = scanner.nextLine();
+
+		System.out.println("Enter contact: ");
+		String contact = scanner.next();
+
+		userService.registerUser(username,password, fullname, contact);
+	}
+
+
 }
