d3a63ccfa0dc92f0ff489125c4f24bae9d706058
Aman Gaade <ak9070777@gmail.com>
2026-07-01 12:59:08 +0530

    APP

--- BookMyTrain/src/IRCTCAPP.java
+++ BookMyTrain/src/IRCTCAPP.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class IRCTCAPP {
+	public static void main(String[] args) {
+
+	}
+
+	private final Scanner scanner = new Scanner();
+	private final UserService userService= new UserService();
+	private final BookingService bookingService =  new BookingService();
+
+
+	//start method
+	public void start(){
+
+		while(true){
+
+			System.out.println("----Welcome to IRCTC app----");
+
+			if(!userService.isLoggedIn()){
+				System.out.println("1. Register");
+				System.out.println("2. LogIn");
+				System.out.println("3. Exit");
+				System.out.println("Enter Choice");
+
+				int choice=scanner.nextInt();
+
+				switch (choice){
+					case 1 -> register();
+				}
+			}
+		}
+	}
 }
